1f832481a31af0c2423470950158c313b106b947
Samuel <chouse.samuel@gmail.com>
2020-10-02 08:48:51 -0400

    Updating files & Grammar

--- models/players.js
+++ models/players.js
@@ -12,16 +12,6 @@ const playerSchema = new mongoose.Schema({
     default: false,
     unique: true,
     required: true
-  },
-  rank: {
-    type: String,
-    default: false,
-    required: true
-  },
-  discord: {
-    type: String,
-    default: false,
-    unique: true
   }
 });
 
